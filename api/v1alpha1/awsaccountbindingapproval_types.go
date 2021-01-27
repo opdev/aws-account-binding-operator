c048a5aaf5fe3a725a92b993b5eb8711bee8da13
Jose R. Gonzalez <josegonzalez89@gmail.com>
2021-01-27 09:42:11 -0600

    adding bindingapproval spec

--- api/v1alpha1/awsaccountbindingapproval_types.go
+++ api/v1alpha1/awsaccountbindingapproval_types.go
@@ -20,23 +20,19 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // AWSAccountBindingApprovalSpec defines the desired state of AWSAccountBindingApproval
 type AWSAccountBindingApprovalSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of AWSAccountBindingApproval. Edit AWSAccountBindingApproval_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// +kubebuilder:validation:Required
+	// AccountID is an AWS Account ID.
+	AccountID string `json:"account_id"`
+	// +kubebuilder:default:false
+	// Approved is whether or not to approve this binding request. To be
+	// set by a cluster administrator.
+	Approved bool `json:"Approved"`
 }
 
 // AWSAccountBindingApprovalStatus defines the observed state of AWSAccountBindingApproval
-type AWSAccountBindingApprovalStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
+type AWSAccountBindingApprovalStatus struct{}
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
