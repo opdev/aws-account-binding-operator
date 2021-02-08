0a74642a23806b8c4034942cdd9f1704a264874e
Jose R. Gonzalez <josegonzalez89@gmail.com>
2021-02-08 14:07:21 -0600

    add missing reconciler registration for approvals

--- main.go
+++ main.go
@@ -94,6 +94,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "AWSAccountBinding")
 		os.Exit(1)
 	}
+	if err = (&controllers.AWSAccountBindingApprovalReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("AWSAccountBindingApproval"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "AWSAccountBindingApproval")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
